d25b040b38c85d0f6dc4523ee8b2d77708530cac
Jukyung <ruluroa@naver.com>
2017-08-30 16:29:09 +0900

    master

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("out");
+			Console.WriteLine("dijf");
+			Console.WriteLine("dijf");
+		}
+		public void state()
+		{
+			int i;
+
+		}
+	}
+}
